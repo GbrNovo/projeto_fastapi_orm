97b37b755896845e516e2f5ede2b0fb2aa9cc2f2
GbrNovo <gabrielnovobrandao@gmail.com>
2026-09-16 08:57:48 -0300

    Implementação do alembic

--- app/models.py
+++ app/models.py
@@ -3,10 +3,15 @@ from sqlalchemy.orm import Mapped, mapped_column
 from app.database import Base
 
 class Endereco(Base):
-  __tablename__ = 'tb_enderecos'
-  id: Mapped[int] = mapped_column(primary_key=True)
-  cep: Mapped[str] = mapped_column(String(10), unique=True)
-  Endereco: Mapped[str] = mapped_column(String(255))
+    __tablename__ = 'tb_enderecos'
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    cep: Mapped[str] = mapped_column(String(10), unique=True)
+    endereco: Mapped[str] = mapped_column(String(255))
+    bairro: Mapped[str] = mapped_column(String(50))
+    cidade: Mapped[str] = mapped_column(String(100))
+    estado: Mapped[str] = mapped_column(String(50))
+    regiao: Mapped[str] = mapped_column(String(50))
 
 
 # FAZER ALUNOS E DISCIPLINAS P/ PRÓXIMA AULA
